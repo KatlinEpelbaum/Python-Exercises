8f6a01fb89d11ec1da25429a7a814eae2f13600e
KatlinEpelbaum <145542776+KatlinEpelbaum@users.noreply.github.com>
2023-11-08 15:12:54 +0200

    Update yl11.py

--- yl11.py
+++ yl11.py
@@ -1,13 +1,11 @@
 user = input("Sissestage lause: ")
-
-if len(user) >= 7 and not len(user) % 2:
- print(user.strip())
- number = len(user)
- middle = int((number- 1) // 2)
- print (user [middle])
- print (user [middle + 1])
- print (user [middle + 2])
+length = len(user)
+middle = int((length - 1) // 2)
+if (length >= 7) and not (length % 2) == 0:
+    print(user.strip())
+    print (user [middle - 1])
+    print (user [middle])
+    print (user [middle + 1])
 
 else:
     print("Sümboleid peab olema rohkem kui 7, paaritus arvus")
-
