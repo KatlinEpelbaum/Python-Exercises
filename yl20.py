03a4c306662eab0f815aa90c5a29835936322229
KatlinEpelbaum <145542776+KatlinEpelbaum@users.noreply.github.com>
2023-11-13 13:07:36 +0200

    Update yl20.py

--- yl20.py
+++ yl20.py
@@ -1,6 +1,5 @@
-x = int(input("Palun sisestage arv: "))
 y = 0
 while y < 13:
-    result = x * y
-    print(x, " * ", y," = ",result) 
+    result = 8 * y
+    print(8, " x ", y," = ",result) 
     y = y + 1
