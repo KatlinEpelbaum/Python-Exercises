f21bfcd08f68baa0ab80a8aeaea5748a4c8bbed9
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-01-22 14:15:25 +0200

    yl28

--- yl28/yl28.py
+++ yl28/yl28.py
@@ -1,10 +1,22 @@
-import urllib3
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-from tqdm import tqdm
 
-url = "http://www.imdb.com/search/title?release_date="
-ourUrl = urllib3.PoolManager().request('GET', url).data
-soup = BeautifulSoup(ourUrl, "lxml")
-print(soup.find('title').text)
+url = "https://editorial.rottentomatoes.com/guide/movies-100-percent-score-rotten-tomatoes/"
 
-    
+req = Request(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0"})
+webpage = urlopen(req).read()
+
+soup = BeautifulSoup(webpage, features="html.parser")
+
+i = 1
+
+movieList = soup.findAll('div', attrs={'class': 'row countdown-item'})
+
+print(movieList)
+for div_item in movieList:
+    div = div_item.find('div', attrs={'class': 'article_movie_title'}).find('h2')
+
+    title = div.find('a').contents[0]
+    year = div.find('span', attrs={'class' : 'subtle start-year'}).contents[0]
+    print(str(i) + '.', title, year)
+    i += 1
