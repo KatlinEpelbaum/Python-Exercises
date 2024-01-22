bc2d1e86918c27bca630ddce75527170f9081605
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-01-22 13:15:10 +0200

    changes

--- yl28/yl28.py
+++ yl28/yl28.py
@@ -3,17 +3,8 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 url = "http://www.imdb.com/search/title?release_date="
-
 ourUrl = urllib3.PoolManager().request('GET', url).data
 soup = BeautifulSoup(ourUrl, "lxml")
 print(soup.find('title').text)
 
-i = 1
-movieList = soup.findAll('div', attrs={'class': 'lister-item mode-advanced'})
-for div_item in tqdm(movieList):
-    div = div_item.find('div',attrs={'class':'lister-item-content'})
-    print (str(i) + '.',)
-    header = div.findChildren('h3',attrs={'class':'lister-item-header'})
-    print('Movie: ' + str((header[0].findChildren('a'))[0].contents[0].encode('utf-8').decode('ascii', 'ignore')))
-    i += 1
     
