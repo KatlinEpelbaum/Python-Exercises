a2c984b492cda193456b8d5f93ef6dc1ac80c2d7
KatlinEpelbaum <katlinepelbaum47@gmail.com>
2024-02-01 14:49:09 +0200

    changes

--- yl13.py
+++ yl13.py
@@ -1,6 +1,8 @@
 animal = input("Sisestage oma lemmikloom: ")
 print(animal[0])
 
-animallist = ["Rott", "hiir", "kass", animal]
+animallist = ["Rott", "hiir", "kass"]
+animallist.append(animal)
 print(animallist)
+
 print(animal[-1])
--- yl13_2nd.py
+++
@@ -1,7 +0,0 @@
-animal = input("Sisestage oma lemmikloom: ")
-print(animal[0])
-
-animallist = ["Rott", "hiir", "kass"]
-animallist.append(animal)
-print(animallist)
-print(animal[-1])
